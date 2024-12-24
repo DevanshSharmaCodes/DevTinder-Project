5595f8b0e1f604ef5549bd4fcadfbac2753190eb
DevanshSharmaCodes <devanshmkv2003@gmail.com>
2024-12-24 09:46:24 +0530

    created some APIs

--- src/app.js
+++ src/app.js
@@ -23,6 +23,58 @@ const app=express();
 app.use(express.json());
 //This is a middleware that parses the incoming request across all routes with JSON payloads. It is used to parse the incoming request body and store it in req.body. Otherwise, req.body will be undefined.
 
+//Fetching details of a single user:
+app.get("/user",async(req,res)=>{
+    const userEmail=req.body.emailId;
+    try {
+        const user=await User.find({emailId:userEmail})
+        //The name of the key should match the name of the field in the database.
+        //This will fetch the user from the database based on the emailId provided in the request body.User.find returns a promise which is resolved when the user is fetched from the database.
+        if(user.length===0){
+            res.status(404).send("User not found");
+        } else{
+            res.send(user);
+        }
+    } catch (error) {
+        res.status(400).send("Error while fetching user");
+    }
+})
+
+//Fetching details of all users:
+app.get("/feed",async(req,res)=>{
+    try {
+        const users=await User.find({})
+        res.send(users);
+    } catch (error) {
+        res.status(400).send("Error while fetching users");
+    }
+})
+
+//Deleting a user by ID:
+app.delete("/user",async(req,res)=>{
+    const userId=req.body.userId;
+    //The ID of the user to be deleted is provided in the request body. 
+    try {
+        await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+    } catch (error) {
+        res.status(400).send("Error while deleting user");
+    }
+})
+
+//Updating a user by ID:
+app.patch("/user",async(req,res)=>{
+    const userId=req.body.userId;
+    const data=req.body;
+    try {
+        await User.findByIdAndUpdate({_id:userId},data);
+        //here data can contain multiple fields to be updated, even those which are not present in our schema(like userId). So, MongoDB will ignore the fields which are not present in the schema and update only the fields which are present.
+        res.send("User updated successfully");
+    } catch (error) {
+        res.status(400).send("Error while updating user");
+    }
+})
+
 app.post("/signup",async (req,res)=>{
     // Creating a new instance of the the User model
     const user=new User(req.body);
